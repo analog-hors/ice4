0162b324bcd5bcff2729663bbcb8e9e85b20d673
analog-hors <44050761+analog-hors@users.noreply.github.com>
2022-11-24 23:04:37 +1000

    Update search.cpp
    size: 3345
    bench: 492958

--- src/search.cpp
+++ src/search.cpp
@@ -84,7 +84,7 @@ struct Searcher {
         if (best >= beta) return best;
 
         int quiets_to_check_table[] = { 0, 13, 14, 23 };
-        int quiets_to_check = depth < 4 ? quiets_to_check_table[depth] : 99;
+        int quiets_to_check = depth > 0 && depth < 4 ? quiets_to_check_table[depth] : 99;
 
         int legals = 0;
         for (int i = 0; i < mvcount; i++) {
