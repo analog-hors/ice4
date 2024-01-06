a31d5716edd2e4240a3c6e5487445fd9dd247bd5
analog-hors <44050761+analog-hors@users.noreply.github.com>
2024-01-07 06:09:13 +1000

    tune eval on big3-ethr dataset
    size: 4047
    bench: 4368187

--- src/eval.cpp
+++ src/eval.cpp
@@ -1,27 +1,27 @@
-// loss: 0.07906    train id: 2024-01-05-17-55-41-frzn-big3
+// loss: 0.05464    train id: 2024-01-07-06-15-47-big3-ethr.mf
 
 #define S(a, b) (a + (b * 0x10000))
 
 int QUADRANTS[] = {
-    S(217, 234), S(229, 239), S(224, 233), S(224, 241),
-    S(236, 301), S(233, 307), S(238, 303), S(234, 306),
-    S(292, 557), S(304, 576), S(299, 550), S(322, 564),
-    S(608, 1094), S(594, 1132), S(611, 1094), S(606, 1144),
+    S(128, 286), S(150, 293), S(136, 290), S(134, 299),
+    S(195, 330), S(197, 339), S(200, 333), S(194, 338),
+    S(244, 635), S(264, 661), S(250, 630), S(281, 652),
+    S(546, 1121), S(525, 1176), S(548, 1133), S(537, 1204),
 };
-int PAWN_SHIELD[] = {S(-3, -8), S(3, -21), S(5, -16), S(13, -10)};
-int MOBILITY[] = {0, S(5, 8), S(1, 0), S(3, 5), S(2, 3), S(1, 4), S(-3, 5)};
+int PAWN_SHIELD[] = {S(-14, -10), S(-4, -21), S(-3, -15), S(7, -12)};
+int MOBILITY[] = {0, S(4, 11), S(-1, 3), S(3, 7), S(1, 3), S(0, 8), S(-4, 7)};
 
-#define DATA_STRING L"7QN621- APG><;.(?IROLB3*FR[ZRF@2Ub{c`_I=A+adGm0B85+,1410$'$#)5,4& ((:A+5759BKE!!6BEIUQCVNBG]w>V?>\\D #3H71+^wai )02+2@>6FHO=;NM\"$ $'30))75-')*7/28? +32)4/1#&*, ),/..33131,,*$$ *9C9600#(6;;1.,)0/1235036426:;<AU@QTHOC~  §ª£1-% %)5.4?4-'/:4SVLA79CIqraNGIVc¡[Tl{ vAACM_\"2/ 2JG3EWWH5<B/ Lfk]luye{p -*)$(*+1134*353!#' '''%\"#&$'))'% \",*#(/.<EA7FNT"
+#define DATA_STRING L"7SK-$&$ ?I@:11$$=JKI?90&EO`^LA</UqeZ`A/GTÄµZAK<@95>>B<-51.:C/2*%56JR7>>AIVZa '>P]`nr0+A+<kKK-$MF &1`U]Bu _bcabtuk~uu37+ 3>=13@=8/:5D116; )0,'8+.!)(- !%.-,1/+40',.)% 2FQC=54*3CAB87407789:><=C99>GIHWjNj|_dY¡®³µ¨1+!! &504;0.$,93[^RF76BFuujSFGYg´[Bs¢·£KZNi$53 4UN5F[WH*682 9U\\M]bkTlu~cy +(-!%'-1355,395 %)#'&&&! %$')-)-(),+ !//5;;8A@D"
 
-#define PAWN_OFFSET S(25, 73)
-#define PASSED_PAWN_OFFSET S(-16, -10)
-#define BISHOP_PAIR S(24, 46)
-#define DOUBLED_PAWN S(5, 14)
-#define TEMPO S(11, 12)
-#define ISOLATED_PAWN S(8, 9)
-#define PROTECTED_PAWN S(7, 7)
-#define ROOK_OPEN S(24, 3)
-#define ROOK_SEMIOPEN S(10, 10)
-#define KING_OPEN S(-44, -3)
-#define KING_SEMIOPEN S(-9, 16)
-#define KING_RING_ATTACKS S(14, -6)
+#define PAWN_OFFSET S(27, 82)
+#define PASSED_PAWN_OFFSET S(-31, -8)
+#define BISHOP_PAIR S(16, 66)
+#define DOUBLED_PAWN S(2, 17)
+#define TEMPO S(13, 17)
+#define ISOLATED_PAWN S(6, 9)
+#define PROTECTED_PAWN S(9, 6)
+#define ROOK_OPEN S(29, 7)
+#define ROOK_SEMIOPEN S(12, 9)
+#define KING_OPEN S(-42, -9)
+#define KING_SEMIOPEN S(-10, 16)
+#define KING_RING_ATTACKS S(20, -4)
