62028e537f134b45102b92686a015f3a1df7ecf0
analog-hors <44050761+analog-hors@users.noreply.github.com>
2024-09-05 20:01:08 +1000

    train for 4x longer
    size: 4091
    bench: 3345756

--- src/eval.cpp
+++ src/eval.cpp
@@ -1,26 +1,26 @@
-// loss: 0.06583    train id: 2024-09-05-19-24-08-frzn-big3-relabelled-partial-1.mf
+// loss: 0.06526    train id: 2024-09-05-19-50-11-frzn-big3-relabelled-partial-1.mf
 
 #define S(a, b) (a + (b * 0x10000))
 
 int QUADRANTS[] = {
-    S(189, 162), S(198, 169), S(197, 164), S(196, 170),
-    S(199, 220), S(194, 226), S(201, 223), S(198, 223),
-    S(261, 414), S(269, 433), S(269, 409), S(291, 422),
-    S(399, 602), S(377, 655), S(398, 643), S(390, 670),
+    S(213, 203), S(221, 209), S(221, 203), S(218, 211),
+    S(232, 257), S(225, 262), S(234, 260), S(230, 260),
+    S(291, 492), S(302, 509), S(300, 484), S(325, 496),
+    S(596, 953), S(577, 989), S(599, 957), S(591, 1002),
 };
-int PAWN_SHIELD[] = {S(-4, -7), S(2, -23), S(4, -19), S(10, -15)};
-int MOBILITY[] = {0, S(4, 9), S(0, 6), S(3, 4), S(1, 3), S(0, 18), S(-4, 4)};
+int PAWN_SHIELD[] = {S(-4, -7), S(2, -24), S(4, -19), S(10, -15)};
+int MOBILITY[] = {0, S(4, 9), S(1, 0), S(3, 4), S(2, 2), S(1, 3), S(-4, 5)};
 
-#define DATA_STRING L"6TO8430 ?TFB?=2*?LRQMC6*DT\\XMG?1Uau^Y[G:*3YvndQP4;38+1344!)&'#.62;& *)5A125/>BPG6.5GGDXZb[RSS^W\\L88T8% 2SN3,z` &.0(0<=3DGN8;NN\"! !$2-)'65-+)*6-14= +13(924&.+2 %*,+045244203,. &6D82).$$465,()&,,+/.0-00./43366LFTWKB<V_b]W_`Z,'$ /*7(4B2+'.92VRJ@68@Jkl^N@GVcuIIb{aok(41$8KJ7@UVC&AF  @WZM[W]S^_e_hhn -/-*)03357;*8<9!#%!(&''! !\"$$(#H=8UJ.<8KC80G7. "
+#define DATA_STRING L";YT<873#DYKEBA4-DQVUQG9-IXa\\RKA4[d{c]^J< (f\\N^8A99*1667$*'%#/73:& *)6D0141?DSI3.6IKI[\\ogNO0lgUQ<;Y7$ 3PB)(rkT %.1(2==3EGN9;ON\"\" \"%3.)(87.,*+8-24> +13(924$-+1 %)-(,1010-*+,$\" );H:5-0&'8;:/++)/0022403424888:<SK]`RIA]} -%\" 1+6(5C2*'-92XUKA68@LopbO@HXe|HIeª])=Ank,95'>RP<F\\]H*GJ  Ljk[mtyg~s -/.)+24589>+:@= #'\"('('! !\"$$($+\"\"/( .01?KJ8GP["
 
-#define PAWN_OFFSET S(18, 66)
-#define PASSED_PAWN_OFFSET S(-19, -6)
-#define BISHOP_PAIR S(28, 42)
-#define TEMPO S(6, 8)
-#define ISOLATED_PAWN S(6, 11)
-#define PROTECTED_PAWN S(6, 7)
-#define ROOK_OPEN S(25, 3)
-#define ROOK_SEMIOPEN S(11, 7)
-#define KING_OPEN S(-42, -3)
-#define KING_SEMIOPEN S(-9, 9)
-#define KING_RING_ATTACKS S(13, -5)
+#define PAWN_OFFSET S(17, 69)
+#define PASSED_PAWN_OFFSET S(-20, -6)
+#define BISHOP_PAIR S(19, 57)
+#define TEMPO S(6, 10)
+#define ISOLATED_PAWN S(7, 11)
+#define PROTECTED_PAWN S(7, 8)
+#define ROOK_OPEN S(24, 4)
+#define ROOK_SEMIOPEN S(10, 8)
+#define KING_OPEN S(-46, -3)
+#define KING_SEMIOPEN S(-10, 10)
+#define KING_RING_ATTACKS S(15, -5)
--- trainer/train.py
+++ trainer/train.py
@@ -69,8 +69,8 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
 print(train_id)
 
-for epoch in range(15):
-    if epoch in [8, 12]:
+for epoch in range(15 * 4):
+    if epoch in [8 * 4, 12 * 4]:
         optimizer.param_groups[0]["lr"] /= 10
 
     iters = 0
