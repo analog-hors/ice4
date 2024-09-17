6b56a8677e47a10cba13c9188965080c15d21ab0
analog-hors <44050761+analog-hors@users.noreply.github.com>
2024-09-17 16:44:36 +1000

    scale up mobility-related terms by 4x
    size: 4063
    bench: 3336150

--- src/eval.cpp
+++ src/eval.cpp
@@ -1,4 +1,4 @@
-// loss: 0.07888    train id: 2024-09-10-22-07-23-frzn-big3
+// loss: 0.07888    train id: 2024-09-17-16-23-36-frzn-big3
 
 #define S(a, b) (a + (b * 0x10000))
 
@@ -9,19 +9,19 @@ int QUADRANTS[] = {
     S(607, 1103), S(594, 1139), S(610, 1103), S(606, 1150),
 };
 int PAWN_SHIELD[] = {S(-4, 7), S(4, -17), S(6, -16), S(14, -11)};
-int MOBILITY[] = {0, S(5, 8), S(1, 0), S(3, 5), S(2, 3), S(1, 4), S(-3, 5)};
+int MOBILITY[] = {0, S(19, 33), S(5, -1), S(13, 20), S(9, 11), S(5, 15), S(-14, 19)};
 int PASSER_RANK[] = {S(-6, -49), S(-13, -32), S(-8, -3), S(4, 30), S(7, 83), S(60, 129)};
 
 #define DATA_STRING L"=VU<982&HTLECA3-FMYVTH80KV``YLE7X_mieRE/ FZR@@>`JIfO&+>K' *WsVb )02+2@>6FHP=;NM\"$ $(30))85.')*7/28? ,32)5/1#&*, ),/..33231,,*%$YZSSX_jbY ZY^`_V!'05)*'+$&..+&&'*/(&$(.,56,%%,16AK4% +9:OTO--125#0, +>;-AMLD:8@4 Lgk]mvzf|p -)*%)++1144*353!#& '''%\"\"&%')('& \",*$)/.=FA7FNT?gZLEGG@? <P[dju"
 
 #define PAWN_OFFSET S(17, 74)
 #define BISHOP_PAIR S(24, 45)
-#define TEMPO S(11, 13)
+#define TEMPO S(42, 51)
 #define ISOLATED_PAWN S(10, 10)
 #define PROTECTED_PAWN S(7, 8)
 #define ROOK_OPEN S(24, 2)
 #define ROOK_SEMIOPEN S(9, 10)
 #define KING_OPEN S(-45, -2)
 #define KING_SEMIOPEN S(-10, 10)
-#define KING_RING_ATTACKS S(13, -5)
+#define KING_RING_ATTACKS S(53, -20)
 #define DIST_OFFSET S(-114, -62)
