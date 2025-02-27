5af2db31307be1086a9f0a7d9d4408c2d6b301fc
analog-hors <44050761+analog-hors@users.noreply.github.com>
2025-02-27 10:34:50 +1000

    attacker corrhist
    size: 4103
    bench: 9272768

--- src/search.cpp
+++ src/search.cpp
@@ -62,7 +62,8 @@ struct Searcher {
             depth -= 2;
         }
 
-        board.movegen(moves, mvcount, depth, mobilities[ply+1]);
+        uint64_t attack_hash;
+        board.movegen(moves, mvcount, depth, mobilities[ply+1], attack_hash);
 
         rep_list[ply] = board.zobrist;
         evals[ply] = board.eval(mobilities[ply+1] - mobilities[ply] + TEMPO)
@@ -70,6 +71,7 @@ struct Searcher {
             + corr_hist[board.stm != WHITE][board.material_hash % CORR_HIST_SIZE] / 198
             + corr_hist[board.stm != WHITE][board.nonpawn_hash[1] % CORR_HIST_SIZE] / 256
             + corr_hist[board.stm != WHITE][board.nonpawn_hash[2] % CORR_HIST_SIZE] / 256
+            + corr_hist[board.stm != WHITE][attack_hash % CORR_HIST_SIZE] / 256
             + (*conthist_stack[ply+1])[0][0] / 102
             + (*conthist_stack[ply])[1][0] / 200;
         int eval = tt_good && tt.eval < 20000 && tt.eval > -20000 ? tt.eval : evals[ply];
@@ -317,6 +319,9 @@ struct Searcher {
                     clamp(best - evals[ply], -CORR_HIST_MAX, CORR_HIST_MAX) * CORR_HIST_UNIT * weight;
                 corr_hist[board.stm != WHITE][board.nonpawn_hash[2] % CORR_HIST_SIZE] =
                     corr_hist[board.stm != WHITE][board.nonpawn_hash[2] % CORR_HIST_SIZE] * (1 - weight) +
+                    clamp(best - evals[ply], -CORR_HIST_MAX, CORR_HIST_MAX) * CORR_HIST_UNIT * weight;
+                corr_hist[board.stm != WHITE][attack_hash % CORR_HIST_SIZE] =
+                    corr_hist[board.stm != WHITE][attack_hash % CORR_HIST_SIZE] * (1 - weight) +
                     clamp(best - evals[ply], -CORR_HIST_MAX, CORR_HIST_MAX) * CORR_HIST_UNIT * weight;
                 (*conthist_stack[ply+1])[0][0] =
                     (*conthist_stack[ply+1])[0][0] * (1 - weight) +
